4810badee72eed748e1b27b8e86a6dda43fc511a
shibe23 <shibe23.info@gmail.com>
2021-03-26 23:05:54 +0900

    refactor: readlineでマッチした行を取得

--- src/add.ts
+++ src/add.ts
@@ -2,7 +2,7 @@ import type {AppOptions, CommandArgs} from "./index";
 import {hasMatchedTextInReadLines, writeFileEffect} from "./lib/readline";
 
 export async function add(value:CommandArgs, appOptions: AppOptions){
-  const {isMatch, line} = await hasMatchedTextInReadLines(appOptions.PORTAL_FILE, (line) => {
+  const {isMatch} = await hasMatchedTextInReadLines(appOptions.PORTAL_FILE, (line) => {
     return line.match(new RegExp(`^${value.label}`)) || []
   })
   if (!isMatch) {
